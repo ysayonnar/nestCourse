cf1e1765cbadfa1f5371220730447204c8c0d0cd
ysayonnar <ysayonnar@gmail.com>
2024-06-22 17:38:08 +0300

    сделал подключение к бд

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { SequelizeModule } from '@nestjs/sequelize'
 
 @Module({
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
+	imports: [
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: 'localhost',
+			port: 5432,
+			username: 'postgres',
+			password: 'root',
+			database: 'nest-course',
+			models: [],
+			autoLoadModels: true,
+		}),
+	],
 })
 export class AppModule {}
