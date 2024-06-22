733e0aa330fae0f7eaa5be56ae51e632935bb4ef
ysayonnar <ysayonnar@gmail.com>
2024-06-22 18:01:47 +0300

    настроил переменные окружения

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
+import { UsersModule } from './users/users.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
 	controllers: [],
 	providers: [],
 	imports: [
+		ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
 		SequelizeModule.forRoot({
 			dialect: 'postgres',
-			host: 'localhost',
-			port: 5432,
-			username: 'postgres',
-			password: 'root',
-			database: 'nest-course',
+			host: process.env.POSTGRES_HOST,
+			port: Number(process.env.POSTGRES_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
 			models: [],
 			autoLoadModels: true,
 		}),
+		UsersModule,
 	],
 })
 export class AppModule {}
