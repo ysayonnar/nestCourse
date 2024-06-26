2893259c298cc1e17c5535c36438f8ff5555cd51
ysayonnar <ysayonnar@gmail.com>
2024-06-27 02:07:24 +0300

    сделал защиту по по ролям

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,8 @@ import { UsersService } from './users.service'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { User } from './users.model'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { Roles } from 'src/auth/roles-auth.decorator'
+import { RolesGuard } from 'src/auth/roles.guard'
 
 @ApiTags('Пользователи')
 @Controller('users')
@@ -19,7 +21,8 @@ export class UsersController {
 
 	@ApiOperation({ summary: 'Получение всех пользователей' })
 	@ApiResponse({ status: 200, type: [User] })
-	@UseGuards(JwtAuthGuard)
+	@Roles('ADMIN')
+	@UseGuards(JwtAuthGuard, RolesGuard)
 	@Get()
 	getAll() {
 		return this.userService.getAllUsers()
